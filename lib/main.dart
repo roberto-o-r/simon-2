e33a1c447c3bae63b5f12916f970afcc8920af20
Roberto Orozco <issc.roberto@outlook.com>
2019-02-01 18:54:13 -0600

    Add circle to animate

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,26 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to Flutter',
       theme: new ThemeData(primaryColor: Colors.black, fontFamily: 'Quantify'),
       home: Scaffold(
-        body: Center(
-          child: Text(
-            'Simon 2',
-            style: TextStyle(
-                color: Colors.white, fontFamily: 'Quantify', fontSize: 45),
-          ),
+        body: Stack(
+          children: <Widget>[
+            Center(
+              child: AnimatedContainer(
+                width: 200,
+                height: 200,
+                decoration:
+                    BoxDecoration(shape: BoxShape.circle, color: Colors.black),
+                duration: Duration(seconds: 1),
+              ),
+            ),
+            Center(
+              child: Text(
+                'Simon 2',
+                style: TextStyle(
+                    color: Colors.white, fontFamily: 'Quantify', fontSize: 45),
+              ),
+            ),
+          ],
         ),
-        backgroundColor: Colors.black,
       ),
     );
   }
