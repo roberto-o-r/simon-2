ca2f5b5b3c5cdaec188e793500b12f21169926f8
Roberto Orozco <issc.roberto@outlook.com>
2019-02-11 17:55:09 -0600

    Refactor based on Vanilla Example (fluttersamples.com)

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,6 @@
-import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
-import 'package:simon_2/animated_circle.dart';
+import 'package:simon_2/app.dart';
 
-void main() => runApp(SimonApp());
-
-class SimonApp extends StatefulWidget {
-  _SimonAppState createState() => _SimonAppState();
-}
-
-class _SimonAppState extends State<SimonApp>
-    with SingleTickerProviderStateMixin {
-  Animation<double> animation;
-  AnimationController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller =
-        AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    animation = Tween<double>(begin: 150, end: 300).animate(controller)
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          controller.reverse();
-        } else if (status == AnimationStatus.dismissed) {
-          controller.forward();
-        }
-      });
-    controller.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) => AnimatedCircle(animation: animation);
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
+void main() {
+  runApp(Simon2App());
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:simon_2/main.dart';
+import 'package:simon_2/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(SimonApp());
+    await tester.pumpWidget(Simon2App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
