d3ba2b0989449e501db94e08ad904b759b30e0f5
Roberto Orozco <issc.roberto@outlook.com>
2019-02-14 17:25:25 -0600

    Add 4 animated cirles for each color

--- lib/widgets/animated_circle.dart
+++ lib/widgets/animated_circle.dart
@@ -2,18 +2,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AnimatedCircle extends AnimatedWidget {
-  AnimatedCircle({Key key, Animation<double> animation})
+  AnimatedCircle({Key key, Animation<double> animation, this.color, this.size})
       : super(key: key, listenable: animation);
+
+  final Color color;
+  final double size;
 
   Widget build(BuildContext context) {
     final Animation<double> animation = listenable;
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10),
-      height: animation.value,
-      width: animation.value,
+      height: animation.value * this.size,
+      width: animation.value * this.size,
       decoration: BoxDecoration(
         shape: BoxShape.circle,
-        color: Colors.red,
+        color: color,
       ),
     );
   }
