7cdafb6e6bb80521f9c7d79d2e6d6dab79a0ded4
Roberto Orozco <issc.roberto@outlook.com>
2019-02-15 14:32:09 -0600

    Refactor AnimatedCircle and add diferent duration to animations

--- lib/widgets/animated_circle.dart
+++ lib/widgets/animated_circle.dart
@@ -1,23 +1,62 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class AnimatedCircle extends AnimatedWidget {
-  AnimatedCircle({Key key, Animation<double> animation, this.color, this.size})
-      : super(key: key, listenable: animation);
-
+class AnimatedCircle extends StatefulWidget {
   final Color color;
-  final double size;
+  final Duration duration;
+  final double scale;
+
+  AnimatedCircle(
+      {@required this.color, @required this.duration, @required this.scale});
+
+  @override
+  _AnimatedCircleState createState() => _AnimatedCircleState();
+}
+
+class _AnimatedCircleState extends State<AnimatedCircle>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _animation;
 
   Widget build(BuildContext context) {
-    final Animation<double> animation = listenable;
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      height: animation.value * this.size,
-      width: animation.value * this.size,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: color,
-      ),
+    _controller.forward();
+
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (BuildContext context, Widget child) {
+        return Container(
+          margin: EdgeInsets.symmetric(vertical: 10),
+          height: _animation.value * widget.scale,
+          width: _animation.value * widget.scale,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: widget.color,
+          ),
+        );
+      },
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(vsync: this, duration: widget.duration);
+    _animation = Tween<double>(begin: 100, end: 110).animate(_controller)
+      ..addStatusListener(handler);
+  }
+
+  void handler(status) {
+    if (status == AnimationStatus.completed) {
+      _controller.reverse();
+    } else if (status == AnimationStatus.dismissed) {
+      _controller.forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
