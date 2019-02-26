d32663f068cc1277e113c9d6f42ebe794ee32021
Roberto Orozco <issc.roberto@outlook.com>
2019-02-26 14:00:58 -0600

    Add colors for disabled button states

--- lib/util/simon_colors.dart
+++ lib/util/simon_colors.dart
@@ -2,8 +2,12 @@ import 'dart:ui';
 
 class SimonColors {
   static const Color green = const Color(0xFF66ff00);
+  static const Color greenDisabled = const Color(0xFFCC66ff00);
   static const Color red = const Color(0xFFff2a2a);
+  static const Color redDisabled = const Color(0xFFCCff2a2a);
   static const Color yellow = const Color(0xFFffcc00);
+  static const Color yellowDisabled = const Color(0xFFCCffcc00);
   static const Color blue = const Color(0xFF00ccff);
+  static const Color blueDisabled = const Color(0xFFCC00ccff);
   static const Color translucent = const Color(0xFF80000000);
 }
