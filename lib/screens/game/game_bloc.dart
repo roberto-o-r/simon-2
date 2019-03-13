21f3489ec026283fc270bd31bb8c656069a8f157
Roberto Orozco <issc.roberto@outlook.com>
2019-03-13 17:52:46 -0600

    Refacto play sound to bloc pattern

--- lib/screens/game/game_bloc.dart
+++ lib/screens/game/game_bloc.dart
@@ -1,12 +1,25 @@
 import 'dart:async';
 import 'dart:math';
 
+import 'package:audioplayers/audio_cache.dart';
 import 'package:bloc/bloc.dart';
 import 'package:simon_2/screens/game/game_event.dart';
 import 'package:simon_2/screens/game/game_state.dart';
 
 class CounterBloc extends Bloc<GameEvent, GameState> {
   Timer _countdown;
+  AudioCache _player1 = new AudioCache(prefix: 'sounds/');
+  AudioCache _player2 = new AudioCache(prefix: 'sounds/');
+  AudioCache _player3 = new AudioCache(prefix: 'sounds/');
+  AudioCache _player4 = new AudioCache(prefix: 'sounds/');
+
+  CounterBloc() {
+    // Load sounds.
+    _player1.load('classic-1.mp3');
+    _player2.load('classic-2.mp3');
+    _player3.load('classic-3.mp3');
+    _player4.load('classic-4.mp3');
+  }
 
   @override
   GameState get initialState => GameState.initial();
@@ -57,7 +70,7 @@ class CounterBloc extends Bloc<GameEvent, GameState> {
 
     // Verify user movement.
     if (currentState.check.first == event.number) {
-      //_playSound(number);
+      _playSound(event.number);
       currentState.check.removeAt(0);
       if (currentState.check.isEmpty) {
         // User has completed all the movements. It's Simon's turn.
@@ -76,7 +89,7 @@ class CounterBloc extends Bloc<GameEvent, GameState> {
   Stream<GameState> _gameOver() async* {
     _countdown.cancel();
     yield currentState.copyWith(locked: true, message: "Game Over");
-    //_playSound(5);
+    _playSound(5);
   }
 
   Stream<GameState> _animateGame() async* {
@@ -89,7 +102,7 @@ class CounterBloc extends Bloc<GameEvent, GameState> {
     for (int n in currentState.game) {
       yield currentState.copyWith(toggled: n);
 
-      //TODO: playsound.
+      _playSound(n);
 
       await _simonWait();
 
@@ -102,6 +115,28 @@ class CounterBloc extends Bloc<GameEvent, GameState> {
 
     // Restart countdown.
     _startCountDown();
+  }
+
+  void _playSound(int number) {
+    switch (number) {
+      case 1:
+        _player1.play('classic-1.mp3');
+        break;
+      case 2:
+        _player2.play('classic-2.mp3');
+        break;
+      case 3:
+        _player3.play('classic-3.mp3');
+        break;
+      case 4:
+        _player4.play('classic-4.mp3');
+        break;
+      default:
+        _player1.play('classic-1.mp3');
+        _player2.play('classic-2.mp3');
+        _player3.play('classic-3.mp3');
+        _player4.play('classic-4.mp3');
+    }
   }
 
   void _startCountDown() {
