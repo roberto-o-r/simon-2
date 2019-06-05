5d5449bc85df25e57132368e205708a5bc76e74f
Roberto Orozco <issc.roberto@outlook.com>
2019-06-05 13:10:07 -0500

    Enable Firebase analytics and crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:simon_2/app.dart';
 
 void main() {
+  // Set `enableInDevMode` to true to see reports while in debug mode.
+  //Crashlytics.instance.enableInDevMode = true;
+
+  // Pass all uncaught errors to Crashlytics.
+  FlutterError.onError = (FlutterErrorDetails details) {
+    Crashlytics.instance.onError(details);
+  };
+
+  // Force crash.
+  //Crashlytics.instance.crash();
+
   runApp(Simon2App());
 }
