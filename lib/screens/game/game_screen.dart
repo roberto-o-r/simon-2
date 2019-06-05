b39c1544e165b7097d386672d3942b47b88a17b0
Roberto Orozco <issc.roberto@outlook.com>
2019-06-05 13:12:26 -0500

    Remove unnecessary widget

--- lib/screens/game/game_screen.dart
+++ lib/screens/game/game_screen.dart
@@ -39,154 +39,147 @@ class _GameScreenState extends State<GameScreen> {
         if (state.play) {
           _playSound(state.toggled);
         }
-
-        return WillPopScope(
-          onWillPop: () {
-            _gameBloc.dispatch(GameOver());
-            Navigator.pop(context);
-          },
-          child: Scaffold(
-            backgroundColor: Colors.black,
-            body: AbsorbPointer(
-              absorbing: state.locked && !state.over,
-              child: Stack(
-                children: <Widget>[
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: FractionallySizedBox(
-                      widthFactor: 0.5,
-                      heightFactor: 0.5,
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                            left: 8, top: 8, right: 4, bottom: 4),
-                        child: RaisedButton(
-                          color: !state.locked || state.toggled == 1
-                              ? SimonColors.green
-                              : SimonColors.greenDisabled,
-                          onPressed: () {
-                            _playSound(1);
-                            _gameBloc.dispatch(UserPlay(1));
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: FractionallySizedBox(
-                      widthFactor: 0.5,
-                      heightFactor: 0.5,
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                            left: 4, top: 8, right: 8, bottom: 4),
-                        child: RaisedButton(
-                          color: !state.locked || state.toggled == 2
-                              ? SimonColors.red
-                              : SimonColors.redDisabled,
-                          onPressed: () {
-                            _playSound(2);
-                            _gameBloc.dispatch(UserPlay(2));
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomLeft,
-                    child: FractionallySizedBox(
-                      widthFactor: 0.5,
-                      heightFactor: 0.5,
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                            left: 8, top: 4, right: 4, bottom: 8),
-                        child: RaisedButton(
-                          color: !state.locked || state.toggled == 3
-                              ? SimonColors.yellow
-                              : SimonColors.yellowDisabled,
-                          onPressed: () {
-                            _playSound(3);
-                            _gameBloc.dispatch(UserPlay(3));
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: FractionallySizedBox(
-                      widthFactor: 0.5,
-                      heightFactor: 0.5,
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                            left: 4, top: 4, right: 8, bottom: 8),
-                        child: RaisedButton(
-                          color: !state.locked || state.toggled == 4
-                              ? SimonColors.blue
-                              : SimonColors.blueDisabled,
-                          onPressed: () {
-                            _playSound(4);
-                            _gameBloc.dispatch(UserPlay(4));
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Container(
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: SimonColors.translucent),
-                        child: Padding(
-                          padding: EdgeInsets.all(12),
-                          child: Text(
-                            "${state.score}",
-                            style: TextStyle(fontSize: 40, color: Colors.white),
-                          ),
-                        )),
-                  ),
-                  Visibility(
-                      visible: state.message == "" ? false : true,
-                      child: GestureDetector(
-                        onTapDown: (TapDownDetails details) {
-                          if (state.over) {
-                            Navigator.pop(context);
-                          }
+        return Scaffold(
+          backgroundColor: Colors.black,
+          body: AbsorbPointer(
+            absorbing: state.locked && !state.over,
+            child: Stack(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: FractionallySizedBox(
+                    widthFactor: 0.5,
+                    heightFactor: 0.5,
+                    child: Padding(
+                      padding:
+                          EdgeInsets.only(left: 8, top: 8, right: 4, bottom: 4),
+                      child: RaisedButton(
+                        color: !state.locked || state.toggled == 1
+                            ? SimonColors.green
+                            : SimonColors.greenDisabled,
+                        onPressed: () {
+                          _playSound(1);
+                          _gameBloc.dispatch(UserPlay(1));
                         },
-                        child: Container(
-                          color: SimonColors.translucent,
-                          child: Stack(
-                            children: <Widget>[
-                              Center(
-                                child: Text(
-                                  state.message,
-                                  style: TextStyle(
-                                    fontSize: 40,
-                                    color: Colors.white,
-                                    fontFamily: 'Quantify',
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: FractionallySizedBox(
+                    widthFactor: 0.5,
+                    heightFactor: 0.5,
+                    child: Padding(
+                      padding:
+                          EdgeInsets.only(left: 4, top: 8, right: 8, bottom: 4),
+                      child: RaisedButton(
+                        color: !state.locked || state.toggled == 2
+                            ? SimonColors.red
+                            : SimonColors.redDisabled,
+                        onPressed: () {
+                          _playSound(2);
+                          _gameBloc.dispatch(UserPlay(2));
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: FractionallySizedBox(
+                    widthFactor: 0.5,
+                    heightFactor: 0.5,
+                    child: Padding(
+                      padding:
+                          EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 8),
+                      child: RaisedButton(
+                        color: !state.locked || state.toggled == 3
+                            ? SimonColors.yellow
+                            : SimonColors.yellowDisabled,
+                        onPressed: () {
+                          _playSound(3);
+                          _gameBloc.dispatch(UserPlay(3));
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: FractionallySizedBox(
+                    widthFactor: 0.5,
+                    heightFactor: 0.5,
+                    child: Padding(
+                      padding:
+                          EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 8),
+                      child: RaisedButton(
+                        color: !state.locked || state.toggled == 4
+                            ? SimonColors.blue
+                            : SimonColors.blueDisabled,
+                        onPressed: () {
+                          _playSound(4);
+                          _gameBloc.dispatch(UserPlay(4));
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: SimonColors.translucent),
+                      child: Padding(
+                        padding: EdgeInsets.all(12),
+                        child: Text(
+                          "${state.score}",
+                          style: TextStyle(fontSize: 40, color: Colors.white),
+                        ),
+                      )),
+                ),
+                Visibility(
+                    visible: state.message == "" ? false : true,
+                    child: GestureDetector(
+                      onTapDown: (TapDownDetails details) {
+                        if (state.over) {
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Container(
+                        color: SimonColors.translucent,
+                        child: Stack(
+                          children: <Widget>[
+                            Center(
+                              child: Text(
+                                state.message,
+                                style: TextStyle(
+                                  fontSize: 40,
+                                  color: Colors.white,
+                                  fontFamily: 'Quantify',
+                                ),
+                              ),
+                            ),
+                            Visibility(
+                              visible: state.over,
+                              child: Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Padding(
+                                  padding: EdgeInsets.only(bottom: 50),
+                                  child: Text(
+                                    "Tap anywhere to go back",
+                                    style: TextStyle(
+                                        fontSize: 18, color: Colors.white),
                                   ),
                                 ),
                               ),
-                              Visibility(
-                                visible: state.over,
-                                child: Align(
-                                  alignment: Alignment.bottomCenter,
-                                  child: Padding(
-                                    padding: EdgeInsets.only(bottom: 50),
-                                    child: Text(
-                                      "Tap anywhere to go back",
-                                      style: TextStyle(
-                                          fontSize: 18, color: Colors.white),
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                          constraints: BoxConstraints.expand(),
+                            )
+                          ],
                         ),
-                      ))
-                ],
-              ),
+                        constraints: BoxConstraints.expand(),
+                      ),
+                    ))
+              ],
             ),
           ),
         );
